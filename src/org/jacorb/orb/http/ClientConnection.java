5e1e78a354d45811510a1a88c832c01b92a34d0c
jacorb <jacorb>
2001-08-08 14:06:26 +0000

    stuff left over from the changes in ClientConnecton and Delegate.

--- src/org/jacorb/orb/http/ClientConnection.java
+++ src/org/jacorb/orb/http/ClientConnection.java
@@ -31,7 +31,7 @@ import org.jacorb.orb.connection.*;
 /**
  *   This class tunnels a GIOP request in HTTP.
  * @author Sebastian Mueller
- * @version $Id: ClientConnection.java,v 1.6 2001-06-13 09:19:00 jacorb Exp $
+ * @version $Id: ClientConnection.java,v 1.7 2001-08-08 14:06:27 jacorb Exp $
  */
 
 public final class ClientConnection 
@@ -81,7 +81,6 @@ public final class ClientConnection
 
 	replies.clear();
 	buffers.clear();
-	objects.clear();
 
 	if( lost_replies > 0 )
 	    org.jacorb.util.Debug.output(2,"Lost " + lost_replies + " outstanding replies");
@@ -102,10 +101,7 @@ public final class ClientConnection
         return buffers;
     }
    
-    public Hashtable get_objects(){
-        return objects;
-    }
-    public Hashtable get_replies(){
+ public Hashtable get_replies(){
         return replies;
     }
 
@@ -224,7 +220,6 @@ public final class ClientConnection
 		    Integer key = new Integer( os.requestId() );
 		    buffers.put( key, os );
 		    replies.put( key, rep );
-		    objects.put( key, o );
 		}
 		
 		synchronized(notifier){
