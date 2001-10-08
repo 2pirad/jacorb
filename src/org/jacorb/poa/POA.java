9b80193a1e4978d1d2236828534a0726b0ce19ec
jacorb <jacorb>
2001-10-08 14:56:44 +0000

    deactivate_object() remove reference from HT createdReferences()

--- src/org/jacorb/poa/POA.java
+++ src/org/jacorb/poa/POA.java
@@ -41,7 +41,7 @@ import java.util.*;
  * The main POA class, an implementation of org.omg.PortableServer.POA
  *
  * @author Reimo Tiedemann, FU Berlin
- * @version $Id: POA.java,v 1.8 2001-10-02 13:51:05 jacorb Exp $
+ * @version $Id: POA.java,v 1.9 2001-10-08 14:56:44 jacorb Exp $
  */
 
 public class POA 
@@ -787,6 +787,19 @@ public class POA
         final ServantActivator servantActivator = 
             useServantManager() ? (ServantActivator)servantManager : null;
 
+        byte[] object_id = POAUtil.maskId(oid);                
+        int pid_length = getPOAId().length;
+        int oid_length = object_id.length;
+        byte [] object_key = new byte[pid_length + oid_length + 1];         
+        int offset = 0;         
+        System.arraycopy(getPOAId(), 0, object_key, offset, pid_length);
+        offset += pid_length;
+        object_key[offset] = POAConstants.OBJECT_KEY_SEP_BYTE;
+        offset++;
+        System.arraycopy(object_id, 0, object_key, offset, oid_length);
+        
+        final String key = new String( object_key );
+        
         final POA thizz = this;
         Thread thread = new Thread() 
         {
@@ -794,6 +807,8 @@ public class POA
             {
                 aom.remove(objectId, requestController, servantActivator, thizz, false);
                 //aom.printSizes();
+
+                createdReferences.remove( key );
             }
         };
         thread.start();
