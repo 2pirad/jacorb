cd2ae72ec3228820385f164aa8d8996539f4ecf7
steve.osselton <steve.osselton>
2002-03-11 18:18:38 +0000

    Update for bug #138

--- src/org/jacorb/poa/Current.java
+++ src/org/jacorb/poa/Current.java
@@ -32,7 +32,7 @@ import java.util.Vector;
  * was invoked and the responsible POA for this object.
  *
  * @author Reimo Tiedemann, FU Berlin
- * @version $Id: Current.java,v 1.8 2001-11-22 15:59:36 prism Exp $
+ * @version $Id: Current.java,v 1.9 2002-03-11 18:18:57 steve.osselton Exp $
  */
 
 public class Current 
@@ -83,10 +83,22 @@ public class Current
         return getInvocationContext().getObjectId();
     }
 
-    public org.omg.PortableServer.POA get_POA() 
+    public org.omg.CORBA.Object get_reference ()
         throws NoContext 
     {
-        return getInvocationContext().getPOA();
+        return get_servant()._this_object (getORB ());
+    }
+
+    public org.omg.PortableServer.Servant get_servant ()
+        throws NoContext 
+    {
+        return getInvocationContext().getServant ();
+    }
+
+    public org.omg.PortableServer.POA get_POA () 
+        throws NoContext 
+    {
+        return getInvocationContext().getPOA ();
     }
     
     synchronized private InvocationContext getInvocationContext() 
@@ -96,11 +108,12 @@ public class Current
 
         Vector cv = (Vector) threadTable.get(ct);
 
-        if (cv != null) {
-			
+        if (cv != null)
+        {
             InvocationContext c = (InvocationContext) cv.lastElement();
 
-            if (c != null) {
+            if (c != null)
+            {
                 return c;
             }
         }
@@ -128,7 +141,3 @@ public class Current
       }
     */
 }
-
-
-
-
