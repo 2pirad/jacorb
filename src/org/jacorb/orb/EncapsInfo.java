848000483d9c9c5583b2945ad1765989dff8a2ec
francisco <francisco>
2002-06-23 16:50:19 +0000

    Changes to save and reset the indirection maps at the beginning of an encapsulation and to restore them at the end of the encapsulation. (CORBA says that "indirections may not cross encapsulation boundaries".)

--- src/org/jacorb/orb/EncapsInfo.java
+++ src/org/jacorb/orb/EncapsInfo.java
@@ -20,12 +20,13 @@ package org.jacorb.orb;
  *   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
+import java.util.Map;
 
 /** 
  * information the has to be saved for each encapsulation and
  * restored later
  * @author Gerald Brose
- * @version $Id: EncapsInfo.java,v 1.4 2002-03-19 09:25:19 nicolas Exp $
+ * @version $Id: EncapsInfo.java,v 1.5 2002-06-23 16:50:19 francisco Exp $
  */
 
 public class EncapsInfo
@@ -34,6 +35,9 @@ public class EncapsInfo
     public int index;
     public int start;
     public int size;
+    public Map valueMap;
+    public Map repIdMap;
+    public Map codebaseMap;
 
     /** constructor used by CDRInputStream */
     
@@ -48,13 +52,21 @@ public class EncapsInfo
     /** 
      * constructor used by CDROutputStream:
      * record the index a new encapsulation starts with
-     * and the start position in the buffer
+     * and the start position in the buffer. CORBA specifies that "indirections
+     * may not cross encapsulation boundaries", so the new encapsulation must
+     * set up its own indirection maps for values, repository ids and codebase 
+     * strings. The maps currently in use are also recorded, to be restored at
+     * the end of the encapsulation.
      */
     
-    public EncapsInfo(int index, int start)
+    public EncapsInfo(int index, int start, 
+                      Map valueMap, Map repIdMap, Map codebaseMap)
     {
 	this.index = index;
 	this.start = start;
+        this.valueMap = valueMap;
+        this.repIdMap = repIdMap;
+        this.codebaseMap = codebaseMap;
     }
 }
 
