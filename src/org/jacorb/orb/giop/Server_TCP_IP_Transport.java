6ee2675fdb33ba4bf31406f4be3d8b9fdcefe5f4
nick.cross <nick.cross>
2002-07-19 14:24:15 +0000

    Added missing shutdownOutput/Input. Rationalised socket/connection_info into TCP_IP_Transport. Added debug message if socket timeout is hit

--- src/org/jacorb/orb/giop/Server_TCP_IP_Transport.java
+++ src/org/jacorb/orb/giop/Server_TCP_IP_Transport.java
@@ -32,16 +32,13 @@ import org.jacorb.util.Debug;
  * Created: Sun Aug 12 20:56:32 2002
  *
  * @author Nicolas Noffke
- * @version $Id: Server_TCP_IP_Transport.java,v 1.8 2002-06-25 08:03:18 gerald Exp $
+ * @version $Id: Server_TCP_IP_Transport.java,v 1.9 2002-07-19 14:24:15 nick.cross Exp $
  */
 
-public class Server_TCP_IP_Transport 
-    extends TCP_IP_Transport 
+public class Server_TCP_IP_Transport
+    extends TCP_IP_Transport
 {
-    private Socket socket = null;
-    private boolean is_ssl = false;
-
-    private String connection_info = null;
+    private boolean is_ssl;
 
     public Server_TCP_IP_Transport( Socket socket,
                                     boolean is_ssl )
@@ -55,7 +52,7 @@ public class Server_TCP_IP_Transport
 
         in_stream = socket.getInputStream();
         out_stream = new BufferedOutputStream(socket.getOutputStream());
-        
+
         connection_info = socket.getInetAddress().getHostName() +
             ':' + socket.getPort();
 
@@ -77,8 +74,22 @@ public class Server_TCP_IP_Transport
         //reestablished.
         if( socket != null )
         {
+            try
+            {
+                java.lang.reflect.Method method
+                = (socket.getClass().getMethod ("shutdownOutput", new Class [0]));
+                method.invoke (socket, new java.lang.Object[0]);
+
+                method = (socket.getClass().getMethod ("shutdownInput", new Class [0]));
+                method.invoke (socket, new java.lang.Object[0]);
+            }
+            catch (Throwable ex)
+            {
+                // If Socket does not support shutdownOutput method (i.e JDK < 1.3)
+            }
+
             socket.close();
-                
+
             //this will cause exceptions when trying to read from
             //the streams. Better than "nulling" them.
             if( in_stream != null )
@@ -90,10 +101,6 @@ public class Server_TCP_IP_Transport
             {
                 out_stream.close();
             }
-
-            //not jdk1.2
-            //socket.shutdownInput();
-            //socket.shutdownOutput();
         }
 
         Debug.output( 2, "Closed connection (server-side) " +
@@ -101,7 +108,7 @@ public class Server_TCP_IP_Transport
 
         throw new CloseConnectionException();
     }
-    
+
     protected void waitUntilConnected()
         throws IOException
     {
@@ -118,5 +125,3 @@ public class Server_TCP_IP_Transport
         return is_ssl;
     }
 }// Server_TCP_IP_Transport
-
-
