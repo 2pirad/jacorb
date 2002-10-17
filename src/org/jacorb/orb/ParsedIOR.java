3b489cf66ed5d4821cbf43364e382862802b5ec1
david.robison <david.robison>
2002-10-17 19:35:32 +0000

    Parse SAS before SSL. Use SAS to determine SSL.

--- src/org/jacorb/orb/ParsedIOR.java
+++ src/org/jacorb/orb/ParsedIOR.java
@@ -40,7 +40,7 @@ import org.omg.CONV_FRAME.*;
  * Class to convert IOR strings into IOR structures
  *
  * @author Gerald Brose, FU Berlin
- * @version $Id: ParsedIOR.java,v 1.39 2002-10-15 13:08:09 nick.cross Exp $
+ * @version $Id: ParsedIOR.java,v 1.40 2002-10-17 19:35:32 david.robison Exp $
  */
 
 public class ParsedIOR
@@ -540,8 +540,16 @@ public class ParsedIOR
 
         int port = pb.port;
 
+        // parse SAS
+        CompoundSecMechList sas = getSASTaggedComponent( pb );
+        if (sas != null)
+        {
+            use_sas = true;
+        }
+
         // bnv: consults SSL tagged component
         SSL ssl = getSSLTaggedComponent( pb );
+        if (sas != null) ssl.target_requires |= sas.mechanism_list[0].target_requires;
 
         // SSL usage is decided the following way: At least one side
         // must require it. Therefore, we first check if it is
@@ -580,6 +588,7 @@ public class ParsedIOR
             ( ((ssl.target_requires & minimum_options) != 0) || //server ...
               ((client_required & minimum_options) != 0))) //...or client require it
         {
+            Debug.output( 1, "Selecting SSL for connection");
             use_ssl = true;
             port = ssl.port;
         }
@@ -594,13 +603,6 @@ public class ParsedIOR
         else
         {
             use_ssl = false;
-        }
-
-        // parse SAS
-        CompoundSecMechList sas = getSASTaggedComponent( pb );
-        if (sas != null)
-        {
-            use_sas = true;
         }
 
         if( port < 0 )
