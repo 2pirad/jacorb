c7d08c90bf7e48cc09452cff568890256e209b31
nicolas <nicolas>
2002-11-26 16:43:47 +0000

    added methods getObjectProperty and getIntPropertyWithDefault

--- src/org/jacorb/util/Environment.java
+++ src/org/jacorb/util/Environment.java
@@ -45,7 +45,7 @@ import java.io.*;
  * so properties from a file found in "." take precedence.
  *
  * @author Gerald Brose
- * @version $Id: Environment.java,v 1.48 2002-10-16 17:02:38 steve.osselton Exp $
+ * @version $Id: Environment.java,v 1.49 2002-11-26 16:43:47 nicolas Exp $
  */
 
 /* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -635,6 +635,59 @@ public class Environment
             throw new Error( "Unable to create int from string >>" +
                              s + "<<. " +
                              "Please check property \"" + key + '\"');
+        }
+    }
+
+    public static int getIntPropertyWithDefault( String key, int def )
+    {
+        String s = _props.getProperty( key );
+
+        if( s != null && s.length() > 0 )
+        {
+            try
+            {
+                return Integer.parseInt( s );
+            }
+            catch( NumberFormatException nfe )
+            {
+                throw new Error( "Unable to create int from string >>" +
+                                 s + "<<. " +
+                                 "Please check property \"" + key + '\"');
+            }
+        }
+        else
+        {
+            return def;
+        }
+    }
+
+    /**
+     * Create an object from the give property. The classes default
+     * constructor will be used.  
+     *
+     * @returns null or an object of the class of the keys value 
+     * @throws Error if reflection fails.
+     */
+    public static Object getObjectProperty( String key )
+    {
+        String s = _props.getProperty( key );
+        
+        if( s != null && s.length() > 0 )
+        {
+            try
+            {
+                Class c = Class.forName( s );
+                return c.newInstance();
+            }
+            catch( Exception e )
+            {
+                throw new Error( "Unable to build class from key >" +
+                                 key +"<: " + e );
+            }
+        }
+        else
+        {
+            return null;
         }
     }
 
