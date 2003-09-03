1bdbe462436abc095a946aba94ca1f6ac4d0113f
brose <brose>
2003-09-03 09:59:28 +0000

    joined in branch, now public (accept) new method

--- src/org/jacorb/idl/Operation.java
+++ src/org/jacorb/idl/Operation.java
@@ -22,7 +22,7 @@ package org.jacorb.idl;
 
 /**
  * @author Gerald Brose
- * @version $Id: Operation.java,v 1.12 2003-09-03 09:53:04 brose Exp $
+ * @version $Id: Operation.java,v 1.13 2003-09-03 09:59:28 brose Exp $
  */
 
 import java.io.PrintWriter;
@@ -59,23 +59,24 @@ public interface Operation
 
     public void print_sendc_Method( PrintWriter ps, String classname );
 
+    public String signature();
+
+
     /**
      * @param printModifiers whether "public abstract" should be added
      */
     void printSignature( PrintWriter ps, boolean printModifiers );
 
-    /** method code for stubs */
+    void printSignature( PrintWriter ps );
 
-    void printMethod( PrintWriter ps, String classname, boolean is_local );
-
-    void print_sendc_Method( PrintWriter ps, String classname );
-
-    /** method code for skeletons */
+    /**
+     * Method code for skeletons
+     * @param ps a <code>PrintWriter</code> value
+     */
 
     void printDelegatedMethod( PrintWriter ps );
 
     void printInvocation( PrintWriter ps );
-
 
     void accept( IDLTreeVisitor visitor );
 
