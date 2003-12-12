8f35a595ba7105bd30539b8071be203a83ca6c2a
gerald <gerald>
2003-12-12 14:31:26 +0000

    (parse) for added robustness, tolerate multiple forward declarations of the same interface name, even after the name has been defined.

--- src/org/jacorb/idl/Interface.java
+++ src/org/jacorb/idl/Interface.java
@@ -22,7 +22,7 @@ package org.jacorb.idl;
 
 /**
  * @author Gerald Brose
- * @version $Id: Interface.java,v 1.51 2003-12-10 09:06:38 gerald Exp $
+ * @version $Id: Interface.java,v 1.52 2003-12-12 14:31:26 gerald Exp $
  */
 
 import java.io.File;
@@ -263,7 +263,7 @@ public class Interface
             // if we get here, there is already a type spec for this interface
             // in the global type table for a forward declaration of this
             // interface. We must replace that table entry with this type spec
-            // if this is not yet another forward declaration
+            // unless this is yet another forward declaration
 
             if (parser.get_pending (full_name ()) != null)
             {
@@ -281,7 +281,8 @@ public class Interface
             }
             else
             {
-                parser.error("Interface " + typeName() + " already defined", token);
+                // this is another forward declaration, ignore
+                //parser.error("Interface " + typeName() + " already defined", token);
             }
         }
 
