e53b95f9558cf81219995d7578e669c4b1953a67
andre.spiegel <andre.spiegel>
2004-01-02 01:50:07 +0000

    Revert collections back to JDK 1.1, since synchronization is important for some of them.

--- src/org/jacorb/poa/RequestController.java
+++ src/org/jacorb/poa/RequestController.java
@@ -39,7 +39,7 @@ import java.util.*;
  * requests out from the queue and will see that the necessary steps are taken.
  *
  * @author Reimo Tiedemann, FU Berlin
- * @version 1.11, 10/26/99, RT $Id: RequestController.java,v 1.26 2003-12-30 15:22:35 andre.spiegel Exp $
+ * @version 1.11, 10/26/99, RT $Id: RequestController.java,v 1.27 2004-01-02 01:50:07 andre.spiegel Exp $
  */
 public final class RequestController
     extends Thread
@@ -60,10 +60,10 @@ public final class RequestController
     private Logger                    logger;
 
     // stores all active requests
-    private Map 			activeRequestTable;
+    private Hashtable 			activeRequestTable;
     // RequestProcessor -> oid
     // for synchronisation with the object deactiviation process
-    private List			deactivationList = new ArrayList();
+    private Vector 			deactivationList = new Vector();
     // oid's
 
     // other synchronisation stuff
@@ -99,8 +99,7 @@ public final class RequestController
 
         requestQueue = new RequestQueue(this, logger);
         activeRequestTable =
-            poa.isSingleThreadModel() ? new HashMap(1) 
-                                      : new HashMap(Environment.threadPoolMax());
+            poa.isSingleThreadModel() ? new Hashtable(1) : new Hashtable(Environment.threadPoolMax());
         getPoolManager();
 
         if( priorityProp != null )
@@ -172,7 +171,7 @@ public final class RequestController
 
     synchronized void freeObject( byte[] oid )
     {
-        deactivationList.remove( new ByteArrayKey( oid ) );
+        deactivationList.removeElement( new ByteArrayKey( oid ) );
     }
 
     AOM getAOM()
@@ -560,7 +559,7 @@ public final class RequestController
 
         ByteArrayKey oidbak = new ByteArrayKey( oid );
 
-        while (activeRequestTable.containsValue(oidbak))
+        while (activeRequestTable.contains(oidbak))
         {
             try
             {
@@ -577,7 +576,7 @@ public final class RequestController
 
         }
 
-        deactivationList.add( oidbak );
+        deactivationList.addElement( oidbak );
     }
 
     /**
