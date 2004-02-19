fda98c79245fbb6a0f5aba039db51dbd627da271
alphonse.bendt <alphonse.bendt>
2004-02-19 10:04:51 +0000

    cleaned up code

--- test/regression/src/org/jacorb/test/notification/bugs/MultipleDeliveryBugTest.java
+++ test/regression/src/org/jacorb/test/notification/bugs/MultipleDeliveryBugTest.java
@@ -41,6 +41,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import org.apache.avalon.framework.logger.Logger;
+import org.jacorb.test.common.TestUtils;
 
 /**
  * Test to reveal bug reported by Matthew Leahy
@@ -48,7 +49,7 @@ import org.apache.avalon.framework.logger.Logger;
  * Under high load Messages were delivered multiple times.
  *
  * @author Alphonse Bendt
- * @version $Id: MultipleDeliveryBugTest.java,v 1.4 2004-02-09 16:26:42 alphonse.bendt Exp $
+ * @version $Id: MultipleDeliveryBugTest.java,v 1.5 2004-02-19 10:04:51 alphonse.bendt Exp $
  */
 
 public class MultipleDeliveryBugTest extends NotificationTestCase
@@ -57,22 +58,16 @@ public class MultipleDeliveryBugTest extends NotificationTestCase
     Logger logger_ = Debug.getNamedLogger(getClass().getName());
 
     EventChannel channel_;
-    EventChannelFactory channelFactory_;
 
     public MultipleDeliveryBugTest (String name, NotificationTestCaseSetup setup)
     {
         super(name, setup);
     }
 
+
     public void setUp() throws Exception
     {
-        channelFactory_ = getFactory();
-
-        Property[] qos = new Property[0];
-        Property[] adm = new Property[0];
-        IntHolder _channelId = new IntHolder();
-
-        channel_ = channelFactory_.create_channel(qos, adm, _channelId);
+        channel_ = getDefaultChannel();
     }
 
 
@@ -85,8 +80,7 @@ public class MultipleDeliveryBugTest extends NotificationTestCase
         NotificationTestCaseSetup _setup =
             new NotificationTestCaseSetup(_suite);
 
-        String[] methodNames =
-            org.jacorb.test.common.TestUtils.getTestMethods(MultipleDeliveryBugTest.class);
+        String[] methodNames = TestUtils.getTestMethods(MultipleDeliveryBugTest.class);
 
         for (int x = 0; x < methodNames.length; ++x)
         {
@@ -132,6 +126,7 @@ public class MultipleDeliveryBugTest extends NotificationTestCase
 
         assertTrue(_receiver.isEventHandled());
     }
+
 
     public static void main(String[] args) throws Exception
     {
