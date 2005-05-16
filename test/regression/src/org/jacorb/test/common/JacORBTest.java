c42e015fdf9fc19fd3a7e4d9fd8b9b47f57a1ead
andre.spiegel <andre.spiegel>
2005-05-16 11:36:43 +0000

    Add copyright header.

--- test/regression/src/org/jacorb/test/common/JacORBTest.java
+++ test/regression/src/org/jacorb/test/common/JacORBTest.java
@@ -1,8 +1,29 @@
 package org.jacorb.test.common;
 
+/*
+ *        JacORB  - a free Java ORB
+ *
+ *   Copyright (C) 2005  Gerald Brose.
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, write to the Free
+ *   Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
+ *   MA 02110-1301, USA.
+ */
+
 /**
  * @author Andre Spiegel spiegel@gnu.org
- * @version $Id: JacORBTest.java,v 1.1 2005-05-16 11:36:00 andre.spiegel Exp $
+ * @version $Id: JacORBTest.java,v 1.2 2005-05-16 11:36:43 andre.spiegel Exp $
  */
 public interface JacORBTest
 {
