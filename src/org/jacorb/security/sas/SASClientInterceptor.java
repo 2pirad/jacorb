dbe5d98bea9a1d4abf98af9f6d3ba6ab1b3a9548
andre.spiegel <andre.spiegel>
2005-06-10 10:13:13 +0000

    (send_request): Be prepared for an empty csmList, fixing bug #603.

--- src/org/jacorb/security/sas/SASClientInterceptor.java
+++ src/org/jacorb/security/sas/SASClientInterceptor.java
@@ -67,7 +67,7 @@ import org.omg.PortableInterceptor.ORBInitInfo;
  * This is the SAS Client Security Service (CSS) Interceptor
  *
  * @author David Robison
- * @version $Id: SASClientInterceptor.java,v 1.25 2005-06-01 11:03:58 andre.spiegel Exp $
+ * @version $Id: SASClientInterceptor.java,v 1.26 2005-06-10 10:13:13 andre.spiegel Exp $
  */
 
 public class SASClientInterceptor
@@ -201,7 +201,9 @@ public class SASClientInterceptor
         }
 
         // get ATLAS tokens
-        AuthorizationElement[] authorizationList = getATLASTokens(orb, csmList);
+        AuthorizationElement[] authorizationList = csmList != null
+                                                   ? getATLASTokens(orb, csmList)
+                                                   : new AuthorizationElement[0];
 
         // establish the security context
         try
